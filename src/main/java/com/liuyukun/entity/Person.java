f8afdd34e2360b1d357b591ce29d046cd66e976a
Lyk19941022 <438396691@qq.com>
2021-01-24 22:10:33 +0800

    first springboot

---
+++ src/main/java/com/liuyukun/entity/Person.java
@@ -0,0 +1,68 @@
+package com.liuyukun.entity;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@ConfigurationProperties(prefix = "person")
+@Component
+public class Person {
+
+    private String name;
+    private Integer age;
+    private String sex;
+    private List<String> likes;
+
+    public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                ", likes=" + likes +
+                '}';
+    }
+
+    public Person(String name, Integer age, String sex, List<String> likes) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.likes = likes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public List<String> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<String> likes) {
+        this.likes = likes;
+    }
+}
